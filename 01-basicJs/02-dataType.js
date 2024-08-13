36fe649b6b6d2505f6b81a2dc8bce69e290cec26
sachkr10 <64580037+sachkr10@users.noreply.github.com>
2024-08-13 18:34:34 +0000

    learning datatypes in JS

---
+++ 01-basicJs/02-dataType.js
@@ -0,0 +1,15 @@
+let age = 10;
+const location = "Kolkata";
+var ditance = 1;
+
+userName = "Sachin"
+// don't use var, use let and constant
+/*
+   Types of primitive data type
+    a) int, bigInt,  String, undefined and null, symbol, bolean
+    b) Object
+
+*/
+let color;
+let response = null
+console.log(typeof response);
