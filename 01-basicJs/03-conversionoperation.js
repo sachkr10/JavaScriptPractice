3d715b164bc2060328fd18ab9e8b3dee3f830b4a
sachkr10 <64580037+sachkr10@users.noreply.github.com>
2024-08-13 19:18:45 +0000

    conversion of datatype in JS

---
+++ 01-basicJs/03-conversionoperation.js
@@ -0,0 +1,31 @@
+let score = "33ab";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let result = Number(score)
+console.log(typeof result);
+console.log(result);
+
+/*
+ "33" => 33
+ "33abc" =>  NaN
+ true => 1, false => 0
+
+*/
+console.log('Boolean Conversion');
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+// 1=> true, 0 => false
+//empty String => false,  anyString => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
